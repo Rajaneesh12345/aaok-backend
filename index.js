0e1d7321401daba09774a522699e47847e53fc55
Rajaneesh Gudivada <rajaneesh1n2345@gmail.com>
2023-02-07 12:36:02 +0530

    bug fix

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api', searchRouter);
 app.use('/', (req, res) => {
 	res.status(200).json({ message: 'Welcome to the AAOK API!' });
 });
-app.use('/api', searchRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
