7829fbbef13a8d3126e91279f93e0bfcb0fa169b
Rajaneesh Gudivada <rajaneesh1n2345@gmail.com>
2023-02-09 12:12:23 +0530

    nice one

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const searchRouter = require('./routes/searchRoutes');
 require('dotenv').config();
+const { getNewConnectionObject } = require('./connection');
 const cors = require('cors');
 
 const app = express();
@@ -13,8 +14,14 @@ app.use('/api', searchRouter);
 app.use('/', (req, res) => {
 	res.status(200).json({ message: 'Welcome to the AAOK API!' });
 });
+const connection = getNewConnectionObject();
+connection.connect(err => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected to database.');
+	}
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Process started on port ${port}.`);
+app.listen(port, () => console.log(`Process started on port ${port}.`));
