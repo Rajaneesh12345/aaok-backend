2b0b44ee604c411586da7d10e83a074542965368
Rajaneesh Gudivada <rajaneesh1n2345@gmail.com>
2023-02-09 12:43:14 +0530

    ..

--- index.js
+++ index.js
@@ -12,16 +12,24 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', searchRouter);
 app.use('/', (req, res) => {
-	res.status(200).json({ message: 'Welcome to the AAOK API!' });
-});
-
-const connection = getNewConnectionObject();
-connection.connect(err => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Connected to database.');
-	}
+	const connection = getNewConnectionObject();
+	connection.connect(err => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Connected to database.');
+			connection.query(
+				'SELECT * FROM picture_links LIMIT 10',
+				(err, result) => {
+					// console.log(result);
+					res.status(200).json({
+						message: 'Welcome to the AAOK API!',
+						result,
+					});
+				}
+			);
+		}
+	});
 });
 
 const port = process.env.PORT || 5000;
